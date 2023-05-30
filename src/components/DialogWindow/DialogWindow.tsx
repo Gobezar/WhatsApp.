452dc2818ba2df3f4d999896533f8a678e128472
Gobezar <vadimkiselevv@yandex.ru>
2023-05-30 21:48:48 +0400

    sec

--- src/components/DialogWindow/DialogWindow.tsx
+++ src/components/DialogWindow/DialogWindow.tsx
@@ -37,15 +37,40 @@ const DialogWindow = () => {
 console.log (currentSubscriber)
 
     return (
-    <div className='dialogWindow'>
+      <div className='dialogWindow'>
       <div className='dialogHeaderDiv'><DialogHeader /></div>
-      {Object.keys(currentSubscriber).length ? <div className='messagesDiv'><Messages /></div> : <></>}
+      {Object.keys(currentSubscriber).length ? (
+        <div className='messagesDiv'>
+          <div className='messagesScroll'>
+            <Messages />
+          </div>
+        </div>
+      ) : (
+        <></>
+      )}
       <div className='buttonsDiv'>
-        <div className='resetDiv'><button disabled={arrayMessages.length ? false : true} onClick={() => dispatch(deleteArrayMessages())} className='resetButton'><DeleteTwoTone twoToneColor="rgba(37, 211, 102, 1)"/></button></div>
-        <div className='inputDiv'><MyInput value={textMessage} onChange={onChangeTextMessage} type="text" placeholder="Текст сообщения..." className='textMessageInput' /></div>
-        <div className='buttonDiv'><button disabled={Object.keys(currentSubscriber).length ? false : true} onClick={toSendMessage} className='myBtn'>Отправить</button></div>
+        <div className='resetDiv'>
+          <button
+            disabled={arrayMessages.length ? false : true}
+            onClick={() => dispatch(deleteArrayMessages())}
+            className='resetButton'
+          >
+            <DeleteTwoTone twoToneColor="rgba(37, 211, 102, 1)"/>
+          </button>
+        </div>
+        <div className='inputDiv'>
+          <MyInput value={textMessage} onChange={onChangeTextMessage} type="text" placeholder="Текст сообщения..." className='textMessageInput' />
+        </div>
+        <div className='buttonDiv'>
+          <button
+            disabled={Object.keys(currentSubscriber).length ? false : true}
+            onClick={toSendMessage}
+            className='myBtn'
+          >
+            Отправить
+          </button>
+        </div>
       </div>
-
     </div>
   )
 }
